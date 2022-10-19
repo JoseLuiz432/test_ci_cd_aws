baa73ef24bac8272e0855340fe120e6232df8a61
joseluiz432 <joseluizmacielp@gmail.com>
2022-10-19 09:30:14 -0300

    Flask minimal

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
+from flask import Flask
 
+app = Flask(__name__)
 
-def main(name):
-  print(f'Hello {name}!')
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 
 if __name__ == '__main__':
-  main('jose')
+  Flask.run(app, host='0.0.0.0', port=8000)
